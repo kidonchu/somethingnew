90ea934c83edbf1dfdbb9da6457a2e7ec33d5270
Kidon Chu <kidonchu@gmail.com>
2016-10-15 00:07:58 -0500

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,84 +1,85 @@
 #!/bin/bash
 set -e
 
-echo "Deploying to gh-pages branch..."
+echo "Installing hugo..."
+go get -u -v github.com/spf13/hugo
 
-exit 0
+echo "Cloning themes..."
+git clone https://github.com/jpescador/hugo-future-imperfect ./themes/hugo-future-imperfect
 
-SOURCE_BRANCH=master
-# Deploy built docs to this branch
-TARGET_BRANCH=gh-pages
+echo "Building static site..."
+hugo
 
-# Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    exit 0
+echo "Deploying to github page branch..."
+
+### Variable definitions
+if [[ -z $SOURCE_BRANCH ]]; then
+	SOURCE_BRANCH="master"
 fi
- 
-# if [ ! -d "$SOURCE_DIR" ]; then
-#   echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
-#   exit 1
-# fi
- 
-# REPO=$(git config remote.origin.url)
-# SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-# SHA=$(git rev-parse --verify HEAD)
+if [[ -z $TARGET_BRANCH ]]; then
+	TARGET_BRANCH="gh-pages"
+fi
+if [[ -z $ENCRYPTION_LABEL ]]; then
+	ENCRYPTION_LABEL="1e2d135be8b5"
+fi
+if [[ -z $SOURCE_DIR ]]; then
+	SOURCE_DIR="public"
+fi
+if [[ -z $GIT_NAME ]]; then
+	GIT_NAME="Kidon Chu"
+fi
+if [[ -z $GIT_EMAIL ]]; then
+	GIT_EMAIL="kidonchu@gmail.com"
+fi
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=$(git rev-parse --verify HEAD)
 
-# echo "Travis Build ID: $TRAVIS_BUILD_ID"
- 
-# if [ -n "$TRAVIS_BUILD_ID" ]; then
-#   # When running on Travis we need to use SSH to deploy to GitHub
-#   #
-#   # The following converts the repo URL to an SSH location,
-#   # decrypts the SSH key and sets up the Git config with
-#   # the correct user name and email (globally as this is a
-#   # temporary travis environment)
-#   #
-#   # Set the following environment variables in the travis configuration (.travis.yml)
-#   #
-#   #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-#   #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-#   #   GIT_NAME         - The Git user name
-#   #   GIT_EMAIL        - The Git user email
-#   #
-#   echo DEPLOY_BRANCH: $DEPLOY_BRANCH
-#   echo GIT_NAME: $GIT_NAME
-#   echo GIT_EMAIL: $GIT_EMAIL
-#   if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-#     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
-#     exit 0
-#   else
-#     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#       echo "Travis should not deploy from pull requests"
-#       exit 0
-#     else
-#       # switch both git and https protocols as we don't know which travis
-#       # is using today (it changed!)
-#       REPO=${REPO/git:\/\/github.com\//git@github.com:}
-#       REPO=${REPO/https:\/\/github.com\//git@github.com:}
-      
-#       chmod 600 $SSH_KEY
-#       eval `ssh-agent -s`
-#       ssh-add $SSH_KEY
-#       git config --global user.name "$GIT_NAME"
-#       git config --global user.email "$GIT_EMAIL"
-#     fi
-#   fi
-# fi
- 
-# REPO_NAME=$(basename $REPO)
-# echo "REPO_NAME: $REPO_NAME"
-# TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
-# echo "TARGET_DIR: $TARGET_DIR"
-# REV=$(git rev-parse HEAD)
-# echo "REV: $REV"
-# echo "Cloning"
-# git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
-# rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
-# cd $TARGET_DIR
-# git add -A .
-# git commit --allow-empty -m "Built from commit $REV"
-# echo "Doing git push $REPO $TARGET_BRANCH"
-# git push $REPO $TARGET_BRANCH
+echo "REPO: ${REPO}"
+echo "SSH_REPO: ${SSH_REPO}"
+echo "SHA: ${SHA}"
 
-# git config --global user.name "$GIT_NAME"
-# git config --global user.email "$GIT_EMAIL"
+### Validations
+# Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	echo "No deploy is needed for pull request; exiting."
+	exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]]; then
+	echo "Travis branch doesn't match with source branch; exiting."
+	exit 0
+fi
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
+  exit 1
+fi
+
+cd public
+
+# If there are no changes to the compiled output
+if [ -z `git diff --exit-code`  ]; then
+	echo "No changes to the output on this push; exiting."
+	exit 0
+fi
+
+git config user.name "$GIT_NAME"
+git config user.email "$GIT_EMAIL"
+
+# Commit generated static site
+git status
+git add -A .
+git commit -m "Deploy to GitHub pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+echo "Adding deploy key to ssh-agent..."
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+echo "Pushing changes to ${REPO} ${TARGET_BRANCH}"
+git push $REPO $TARGET_BRANCH
